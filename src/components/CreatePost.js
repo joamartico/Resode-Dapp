e9dd2f931454c42d1a3d27c5e88b9bd4d10569ae
joamartico <joamartico@gmail.com>
2022-03-15 17:52:49 -0300

    all CreatePost component in Main (IonPage)

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,4 +1,4 @@
-import { IonInput, IonTextarea, useIonToast } from '@ionic/react';
+import { IonContent, IonInput, IonPage, IonTextarea, useIonToast } from '@ionic/react';
 import React, { useState } from 'react';
 import { useMoralisFile } from 'react-moralis';
 import styled from 'styled-components';
@@ -43,8 +43,9 @@ const CreatePost = () => {
     const contentUri = `https://ipfs.io/ipfs/${result.path}`;
     return contentUri;
   };
-
-  async function onSubmit() {
+  // quizas redirecciona a appstore como un link porque no esta en ionpage
+  async function onSubmit(e) {
+    e.prevent.default();
     if (title == '' || text == '') {
       present({
         message: 'Please fill all fields',
@@ -64,36 +65,40 @@ const CreatePost = () => {
         setText('');
         setTitle('');
       })
-      .catch(console.log)
+      .catch(console.log);
   }
 
   return (
-    <Card>
-      <Padding>
-        <div>
-          <InputText
-            rows={1}
-            placeholder="Title of your post..."
-            autoGrow
-            value={title}
-            onIonChange={e => setTitle(e.detail.value)}
-          />
-          <InputText
-            rows={3}
-            placeholder="Text of your post..."
-            autoGrow
-            value={text}
-            onIonChange={e => setText(e.detail.value)}
-          />
-        </div>
-        <Button
-          onClick={onSubmit}
-          background="linear-gradient(174deg, rgba(93,152,255,1) 0%,  rgba(0,63,255,1) 80%, rgba(12,102,222,1) 100%)"
-        >
-          Post
-        </Button>
-      </Padding>
-    </Card>
+    <IonPage>
+      <IonContent>
+        <Card>
+          <Padding>
+            <div>
+              <InputText
+                rows={1}
+                placeholder="Title of your post..."
+                autoGrow
+                value={title}
+                onIonChange={e => setTitle(e.detail.value)}
+              />
+              <InputText
+                rows={3}
+                placeholder="Text of your post..."
+                autoGrow
+                value={text}
+                onIonChange={e => setText(e.detail.value)}
+              />
+            </div>
+            <Button
+              onClick={onSubmit}
+              background="linear-gradient(174deg, rgba(93,152,255,1) 0%,  rgba(0,63,255,1) 80%, rgba(12,102,222,1) 100%)"
+            >
+              Post
+            </Button>
+          </Padding>
+        </Card>
+      </IonContent>
+    </IonPage>
   );
 };
 
--- src/screens/Main.js
+++ src/screens/Main.js
@@ -5,6 +5,8 @@ import {
   IonList,
   IonRefresher,
   IonRefresherContent,
+  IonTextarea,
+  useIonToast,
 } from '@ionic/react';
 
 import styled from 'styled-components';
@@ -15,12 +17,19 @@ import CreatePost from '../components/CreatePost';
 import GreaterExponents from '../components/GreaterExponents';
 import NativeBalance from '../components/NativeBalance';
 import Post from '../components/Post';
-import { Padding, Row, Scroll } from '../components/StyledComponents';
+import { Button, Card, Padding, Row, Scroll } from '../components/StyledComponents';
 import YourReputation from '../components/YourReputation';
 import useGlobalState from '../hooks/useGlobalState';
 import { isMobile } from '../helpers/isMobile';
 import useQuery from '../hooks/useQuery';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { create } from "ipfs-http-client";
+
+const ipfs = create({
+  host: 'ipfs.infura.io',
+  port: '5001',
+  protocol: 'https',
+});
 
 const Main = () => {
   const { selectedCategory, setSelectedCategory, resodeContract, walletAddress, chainId } =
@@ -60,6 +69,62 @@ const Main = () => {
   });
 
   // if (!resodeContract) return null;
+  // 
+  // 
+  // 
+  // 
+  // 
+  // 
+  const [title, setTitle] = useState('');
+  const [text, setText] = useState('');
+  const [present] = useIonToast();
+
+  const processContent = async () => {
+    const content = {
+      title,
+      text,
+    };
+    // const IPFSResult = await IPFSProcessor.saveFile(
+    //   'post.json',
+    //   { base64: btoa(JSON.stringify(content)) },
+    //   { saveIPFS: true }
+    // );
+    // await alert('Content: ', IPFSResult._ipfs);
+    // return IPFSResult._ipfs;
+
+    const result = await ipfs.add(JSON.stringify(content));
+
+    // const result = [];
+
+    // for await (const _result of ipfs.add(content)) {
+    //   result.push(_result);
+    // }
+    const contentUri = `https://ipfs.io/ipfs/${result.path}`;
+    return contentUri;
+  };
+  // quizas redirecciona a appstore como un link porque no esta en ionpage
+  async function onSubmit() {
+    if (title == '' || text == '') {
+      present({
+        message: 'Please fill all fields',
+        duration: 2000,
+        color: 'danger',
+      });
+      return null;
+    }
+    const contentURI = await processContent();
+
+    console.log('resodeContract on post', resodeContract);
+
+    resodeContract.methods
+      .createPost(selectedCategory.id, '0x91', contentURI)
+      .send({ from: walletAddress, gas: 3000000 })
+      .then(() => {
+        setText('');
+        setTitle('');
+      })
+      .catch(console.log);
+  }
 
   return (
     <IonPage>
@@ -93,8 +158,33 @@ const Main = () => {
         <Scroll pt={isMobile && '32%'} pb="60px">
           <Row>
             <Col70>
-              <CreatePost />
-              {/* {console.log('posts: ', posts)} */}
+              {/* <CreatePost /> */}
+              <Card>
+                <Padding>
+                  <div>
+                    <InputText
+                      rows={1}
+                      placeholder="Title of your post..."
+                      autoGrow
+                      value={title}
+                      onIonChange={e => setTitle(e.detail.value)}
+                    />
+                    <InputText
+                      rows={3}
+                      placeholder="Text of your post..."
+                      autoGrow
+                      value={text}
+                      onIonChange={e => setText(e.detail.value)}
+                    />
+                  </div>
+                  <Button
+                    onClick={onSubmit}
+                    background="linear-gradient(174deg, rgba(93,152,255,1) 0%,  rgba(0,63,255,1) 80%, rgba(12,102,222,1) 100%)"
+                  >
+                    Post
+                  </Button>
+                </Padding>
+              </Card>
               {posts?.map(post => (
                 <Post
                   key={post.postId}
@@ -124,6 +214,16 @@ const Main = () => {
 
 export default Main;
 
+const InputText = styled(IonTextarea)`
+  background: #eaeaea;
+  border-radius: 14px;
+  padding: ${isMobile ? '3%' : '2%'};
+  margin-top: 0 !important;
+  --padding-bottom: 0px !important;
+  --padding-top: 0px !important;
+  margin-bottom: 10px !important;
+`;
+
 const Col70 = styled.div`
   width: ${isMobile ? '100%' : '56%'};
   /* width: 100%; */
@@ -142,7 +242,7 @@ const Col30 = styled.div`
   height: 600px;
   background-color: #fff;
   border-radius: 14px;
-  box-shadow: 50px 0 100px 50px rgba(209, 224, 255, 0.5); 
+  box-shadow: 50px 0 100px 50px rgba(209, 224, 255, 0.5);
 `;
 
 const ToolbarButton = styled.div`
